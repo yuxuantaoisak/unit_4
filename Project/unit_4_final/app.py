892d087b2b5d7966517e56b85c0e7a74ba1809bf
Yuxuan Tao <144768397+yuxuantaoisak@users.noreply.github.com>
2024-06-04 15:05:41 +0900

    Add files via upload

---
+++ Project/unit_4_final/app.py
@@ -0,0 +1,423 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory
+from my_lib import database_worker, encrypt_password, check_password
+from werkzeug.utils import secure_filename
+from functools import wraps
+import jwt
+import pycountry
+import dotenv
+import os
+from datetime import datetime, timedelta
+
+app = Flask(__name__)  # Initialize Flask application
+app.secret_key = 'you-will-never-guess'  # Define a secret key for the application.
+app.config['UPLOAD_FOLDER'] = 'upload_folder'  # Define the folder where user uploads (such as images) will be stored
+
+
+UPLOAD_FOLDER = "upload_folder"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+dotenv.load_dotenv()  # Load environment variables from .env file
+token_key = 'TOKEN_ENCRYPTION_KEY'  # Retrieve the token encryption key from environment variables
+
+
+# Define a function to parse dates and return only the date part (excluding time)
+def parse_date(value):
+    return value.split(' ')[0]
+
+
+# Define a function to calculate how much time has passed since a given datetime
+def how_much_time_ago(value):
+    now = datetime.now()
+    post_date = datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f')
+    time_diff = now - post_date
+
+    # Define a list of time units and their lengths in seconds
+    time_units = [
+        ('year', 60*60*24*365),
+        ('month', 60*60*24*30),
+        ('week', 60*60*24*7),
+        ('day', 60*60*24),
+        ('hour', 60*60),
+        ('minute', 60),
+        ('second', 1)
+    ]
+
+    # Loop through each time unit, checking if the time difference is greater than the unit length
+    for unit, unit_length in time_units:
+        if time_diff.total_seconds() > unit_length:
+            num_units = int(time_diff.total_seconds() // unit_length)
+            unit = unit if num_units == 1 else unit + 's'  # Handle plural units
+            return f"{num_units} {unit} ago"
+
+    return "Just now"
+
+
+# Define a function to convert country names to their corresponding flag code
+def flag(country):
+    try:
+        return pycountry.countries.get(name=country).alpha_2.lower()
+    except:
+        return 'unknown'
+
+
+# Register the custom functions as filters in Jinja2 environment
+app.jinja_env.filters['parse_date'] = parse_date
+app.jinja_env.filters['how_much_time_ago'] = how_much_time_ago
+app.jinja_env.filters['flag'] = flag
+
+
+# Define a decorator function for routes that require a user to be logged in
+def token_required(f):
+    # Use the wraps function from functools to preserve the original function's name and docstring
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        # If there is no token in the user's session, redirect them to the login page
+        if 'token' not in session:
+            return redirect(url_for('login'))
+        try:
+            # Attempt to decode the token using the secret key
+            # If the token is valid, this will return the data encoded in the token
+            data = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+        except:
+            # If an error occurs (such as if the token is expired or tampered with), redirect the user to the login page
+            return redirect(url_for('login'))
+
+        # If the token is valid, continue to the original function with the original arguments
+        return f(*args, **kwargs)
+
+    # Return the new decorated function
+    return decorated
+
+
+@app.route('/like_post/<int:post_id>', methods=['POST'])
+@token_required
+def like_post(post_id):
+    current_user = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+    user_id = current_user['user_id']
+
+    db = database_worker("shelfshare.db")
+    check_like_query = f"SELECT * FROM likes WHERE user_id = {user_id} AND post_id = {post_id}"
+    like_record = db.run_fetchone(query=check_like_query)
+
+    # count the number of times a post_id appears in the like table
+    count_likes_query = f"SELECT COUNT(*) FROM likes WHERE post_id = {post_id}"
+
+    if like_record:
+        # If the like record exists, delete it (unlike)
+        unlike_query = f"DELETE FROM likes WHERE user_id = {user_id} AND post_id = {post_id}"
+        db.run_save(query=unlike_query)
+
+    else:
+        # If the like record doesn't exist, create it (like)
+        like_query = f"INSERT INTO likes (user_id, post_id) VALUES ('{user_id}', '{post_id}')"
+        db.run_save(query=like_query)
+
+    return redirect('/post/' + str(post_id))
+
+
+@app.route('/follow_post/<int:post_id>', methods=['POST'])
+@token_required
+def follow_post(post_id):
+    current_user = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+    user_id = current_user['user_id']
+
+    db = database_worker("shelfshare.db")
+    check_like_query = f"SELECT * FROM following WHERE user_id = {user_id} AND post_id = {post_id}"
+    following_record = db.run_fetchone(query=check_like_query)
+
+    # count the number of times a post_id appears in the like table
+    count_likes_query = f"SELECT COUNT(*) FROM following WHERE post_id = {post_id}"
+
+    if following_record:
+        # If the follow record exists, delete it (unfollow)
+        unfollow_query = f"DELETE FROM following WHERE user_id = {user_id} AND post_id = {post_id}"
+        db.run_save(query=unfollow_query)
+
+    else:
+        # If the like record doesn't exist, create it (like)
+        following_query = f"INSERT INTO following (user_id, post_id) VALUES ('{user_id}', '{post_id}')"
+        db.run_save(query=following_query)
+
+    return redirect('/post/' + str(post_id))
+
+
+@app.route('/delete_comment/<int:comment_id>')
+@token_required
+def delete_comment(comment_id):
+    db = database_worker("shelfshare.db")
+    delete_comment = f"DELETE FROM comments WHERE id = {comment_id}"
+    db.run_save(query=delete_comment)
+    return redirect('/profile')
+
+
+@app.route('/delete_post/<int:post_id>')
+@token_required
+def delete_post(post_id):
+    db = database_worker("shelfshare.db")
+    delete_post = f"DELETE FROM posts WHERE id = {post_id}"
+    db.run_save(query=delete_post)
+    return redirect('/profile')
+
+
+@app.route('/edit_comment/<int:comment_id>', methods=['GET', 'POST'])
+@token_required
+def edit_comment(comment_id):
+    db = database_worker("shelfshare.db")
+    comment = db.search(f"SELECT * FROM comments WHERE id = {comment_id}")
+
+    if request.method == 'POST':
+        content = request.form['content']
+
+        query = f"UPDATE comments SET content='{content}'"
+        db.run_save(query=query)
+        return redirect(url_for('profile'))
+
+    return render_template('edit_comment.html', comment=comment[0])
+
+
+@app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
+@token_required
+def edit_post(post_id):
+    db = database_worker("shelfshare.db")
+    post = db.search(f"SELECT * FROM posts WHERE id = {post_id}")
+    if request.method == 'POST':
+        title = request.form['title']
+        category = request.form['category']
+        content = request.form['content']
+        image = request.files.get('image')
+
+        # If an image was uploaded, save it and get its filename
+        if image:
+            image_name = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
+        else:
+            image_name = None
+
+        # SQL query to update the post data in the database
+        query = f"UPDATE posts SET title='{title}', category='{category}', content='{content}', image_name='{image_name}' WHERE id={post_id}"
+        db.run_save(query)
+        return redirect(url_for('profile'))
+    return render_template('edit_post.html', post=post[0])
+
+
+@app.route('/delete_profile/<int:user_id>')
+@token_required
+def delete_profile(user_id):
+    db = database_worker("shelfshare.db")
+    db.run_save(f"DELETE FROM posts WHERE user_id = {user_id}")
+    db.run_save(f"DELETE FROM comments WHERE user_id = {user_id}")
+    db.run_save(f"DELETE FROM users WHERE id = {user_id}")
+    return redirect('/logout')
+
+
+@app.route('/profile')
+@token_required
+def profile():
+    current_user = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+    user_id = current_user['user_id']
+    db = database_worker("shelfshare.db")
+    user = db.search(f"SELECT * FROM users WHERE id = {user_id}")
+    posts = db.search(f"SELECT * FROM posts WHERE user_id = {user_id} ORDER BY posts.datetime DESC")
+    comments = db.search(f"SELECT * FROM comments WHERE user_id = {user_id} ORDER BY comments.datetime DESC")
+    liked_posts = db.search(f"SELECT posts.* FROM likes INNER JOIN posts ON likes.post_id = posts.id WHERE likes.user_id = {user_id}")
+    return render_template('profile.html', posts=posts, comments=comments, liked_posts=liked_posts, user=user[0])
+
+
+@app.route('/add_comment/<int:post_id>', methods=['POST'])
+@token_required
+def add_comment(post_id):
+    db = database_worker("shelfshare.db")
+    if request.method == 'POST':
+        comment = request.form['comment']
+        user_id = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+        create_comment = f"INSERT INTO comments (content, datetime, user_id, post_id) VALUES ('{comment}', '{datetime.now()}', '{user_id['user_id']}', '{post_id}')"
+        db.run_save(query=create_comment)
+    return redirect('/post/' + str(post_id))
+
+
+@app.route('/post/<int:post_id>')
+@token_required
+def view_post(post_id):
+    current_user = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+    db = database_worker("shelfshare.db")
+    post = db.search(f"SELECT posts.*, users.name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.id = {post_id}")
+    comments = db.search(f"SELECT comments.*, users.name FROM comments INNER JOIN users ON comments.user_id = users.id WHERE comments.post_id = {post_id} ORDER BY comments.datetime DESC")
+
+    # Check if the current user has liked the post
+    user_like = db.run_fetchone(f"SELECT * FROM likes WHERE post_id = {post_id} AND user_id = {current_user['user_id']}")
+    user_follow = db.run_fetchone(f"SELECT * FROM following WHERE post_id = {post_id} AND user_id = {current_user['user_id']}")
+    is_liked = user_like is not None
+    is_followed = user_follow is not None
+
+    if post:
+        return render_template('post.html', post=post[0], comments=comments, is_liked=is_liked, is_followed=is_followed)
+
+    return render_template('index.html')
+
+
+@app.route('/create_post', methods=['GET', 'POST'])
+@token_required
+def create_post():
+    if 'image' in request.files:
+        image = request.files['image']
+        if image.filename != '':
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        else:
+            filename = None
+    else:
+        filename = None
+
+    current_user = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+    db = database_worker("shelfshare.db")
+
+    if request.method == 'POST':
+        title = request.form['title']
+        category = request.form['category']
+        content = request.form['content']
+        user_id = current_user['user_id']
+
+        if category == 'Restaurants':
+            rating = request.form['rating']
+            tags = ','.join(request.form.getlist('tags'))
+            create_post = f"INSERT INTO posts (title, category, content, datetime, image_name, user_id, rating, tags) VALUES ('{title}', '{category}', '{content}', '{datetime.now()}', '{filename}','{user_id}', '{rating}', '{tags}')"
+            db.run_save(query=create_post)
+        else:
+            create_post = f"INSERT INTO posts (title, category, content, datetime, image_name, user_id, rating, tags) VALUES ('{title}', '{category}', '{content}', '{datetime.now()}', '{filename}','{user_id}', 0, 'None')"
+            db.run_save(query=create_post)
+
+        return redirect(url_for('index'))
+
+    return render_template('create_post.html')
+
+
+@app.route('/file/<filename>')
+def file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/following')
+@token_required
+def following():
+    current_user = jwt.decode(session['token'], token_key, algorithms=['HS256'])
+    user_id = current_user['user_id']
+    db = database_worker("shelfshare.db")
+    user = db.search(f"SELECT * FROM users WHERE id = {user_id}")
+    comments = db.search(f"SELECT * FROM comments WHERE user_id = {user_id} ORDER BY comments.datetime DESC")
+    followed_posts = db.search(
+        f"SELECT posts.* FROM following INNER JOIN posts ON following.post_id = posts.id WHERE following.user_id = {user_id}")
+    return render_template('following.html', comments=comments, followed_posts=followed_posts, user=user[0])
+
+
+@app.route('/recipes')
+@token_required
+def recipes():
+    db = database_worker("shelfshare.db")
+    posts = db.search("SELECT posts.*, users.name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.category = 'Recipe' ORDER BY posts.datetime DESC")
+    return render_template('recipes.html', posts=posts)
+
+
+@app.route('/locations')
+@token_required
+def locations():
+    db = database_worker("shelfshare.db")
+    posts = db.search("SELECT posts.*, users.name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.category = 'Location' ORDER BY posts.datetime DESC")
+    return render_template('locations.html', posts=posts)
+
+
+@app.route('/restaurants')
+@token_required
+def restaurants():
+    db = database_worker("shelfshare.db")
+    posts = db.search("SELECT posts.*, users.name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.category = 'Restaurants' ORDER BY posts.datetime DESC")
+    return render_template('restaurants.html', posts=posts)
+
+
+@app.route('/top_liked')
+@token_required
+def top_liked():
+    db = database_worker("shelfshare.db")
+    most_liked_posts = db.search("SELECT posts.*, users.name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.id IN (SELECT post_id FROM likes GROUP BY post_id ORDER BY COUNT(*) DESC)")
+    return render_template('top_liked.html', most_liked_posts=most_liked_posts)
+
+
+@app.route('/')
+@token_required
+def index():
+    db = database_worker("shelfshare.db")
+    posts = db.search("SELECT posts.*, users.name FROM posts INNER JOIN users ON posts.user_id = users.id ORDER BY posts.datetime DESC")
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if email and password fields are not empty
+        if len(email) > 0 and len(password) > 0:
+            # Create a database worker to interact with the database
+            db = database_worker("shelfshare.db")
+            # SQL query to get user information by email
+            login = f"SELECT * FROM users WHERE email = '{email}'"
+            # Fetch user information from database
+            user = db.search(query=login)
+            # Check if user exists
+            if user:
+                # Unpack user data
+                id, name, username, email, hash = user[0]
+                # Check if password is correct
+                if check_password(user_password=password, hashed_password=hash):
+                    # Create a new JWT token for the user
+                    token = jwt.encode({'user_id': id, 'exp': datetime.utcnow() + timedelta(minutes=180)}, token_key, algorithm='HS256')
+                    # Store the JWT token in the user session
+                    session['token'] = token
+                    # Redirect the user to the home page
+                    return redirect("/")
+                else:
+                    # Flash a message indicating that the password is incorrect
+                    flash(('Login failed. Password Incorrect', 'danger'))
+            else:
+                # Flash a message indicating that the login credentials are incorrect
+                flash(('Login failed. Incorrect Credentials', 'danger'))
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    db = database_worker("shelfshare.db")
+
+    if request.method == "POST":
+        name = request.form['fullname']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        password_confirm = request.form['password_confirm']
+        if len(name) > 0 and len(username) > 0 and len(email) > 0 and len(password) > 0:
+            if password != password_confirm:
+                flash(('Passwords do not match.', 'danger'))
+
+            elif len(password) < 8:
+                flash(('Password must be at least 8 characters.', 'danger'))
+
+            else:
+                register = f"INSERT into users(name, username, email, password) values('{name}', '{username}', '{email}', '{encrypt_password(password)}')"
+                db.run_save(query=register)
+                flash(('Registration completed. Please log in.', 'success'))
+                return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('token', None)
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ Project/unit_4_final/my_lib.py
@@ -0,0 +1,40 @@
+import sqlite3
+from passlib.hash import sha256_crypt
+
+
+class database_worker:
+    def __init__(self, name):
+        self.connection = sqlite3.connect(name)
+        self.cursor = self.connection.cursor()
+
+    def search(self, query):
+        result = self.cursor.execute(query).fetchall()
+        return result
+
+    def run_save(self, query):
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
+
+    def run_fetchone(self, query):
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result
+
+
+# code for the password hashing
+
+
+# Create an object of the class Crypt
+hasher = sha256_crypt.using(rounds=30000)
+
+
+# This function receives the unsafe password and returns the hashed password
+def encrypt_password(user_password):
+    return hasher.encrypt(user_password)
+
+
+def check_password(hashed_password, user_password):
+    return hasher.verify(user_password, hashed_password)
